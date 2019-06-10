e51389db2bf6d2f6e533426de6e2ac24264bb159
David Meister <thedavidmeister@gmail.com>
2019-06-10 21:07:46 +1000

    initial commit from core

---
+++ app-spec/src/test.nix
@@ -0,0 +1,12 @@
+let
+  pkgs = import ../../nixpkgs/nixpkgs.nix;
+
+  name = "hc-app-spec-test";
+
+  script = pkgs.writeShellScriptBin name ''
+  hc-cli-install;
+  hc-conductor-rust-install;
+  (cd app_spec && ./build_and_test.sh);
+  '';
+in
+script
---
+++ cli/src/uninstall.nix
@@ -0,0 +1,12 @@
+let
+  pkgs = import ../../nixpkgs/nixpkgs.nix;
+
+  name = "hc-cli-uninstall";
+
+  script = pkgs.writeShellScriptBin name
+  ''
+   echo "dropping hc binary from cargo home directory"
+   rm -f $CARGO_HOME/bin/hc
+  '';
+in
+script
