990445c9c2e0d56ca3c41f54d80a31b95d8208d2
David Meister <thedavidmeister@gmail.com>
2019-06-26 18:42:43 +1000

    add circle config

--- nixpkgs/nixpkgs.nix
+++ nixpkgs/nixpkgs.nix
@@ -2,18 +2,18 @@ let
   # https://vaibhavsagar.com/blog/2018/05/27/quick-easy-nixpkgs-pinning/
   inherit (import <nixpkgs> {}) fetchgit;
 
-  # nixos holo-host channel @ 2019-04-02
+  # nixos channel latest 19.03 2019-06-26
   channel-holo-host = fetchgit {
-   url = "https://github.com/NixOs/nixpkgs.git";
-   rev = "40baa1f2de4abda35dc93d5dacedcccc2d72238a";
-   sha256 = "19sksk1i7q92x7h8q655l68ykrgknmxc3drlk0cjzjn2mp3np5xs";
+   url = "https://github.com/NixOs/nixpkgs-channels.git";
+   rev = "8634c3b619909db7fc747faf8c03592986626e21";
+   sha256 = "0hcpy4q64vbqmlmnfcavfxilyygyzpwdsss8g3p73ikpic0j6ziq";
   };
 
   # the mozilla rust overlay
   # allows us to track cargo nightlies in a nixos friendly way
   # avoids rustup
   # not compatible with parallel rustup installation
-  moz-overlay = import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/50bae918794d3c283aeb335b209efd71e75e3954.tar.gz);
+  moz-overlay = import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/tarball/50bae918794d3c283aeb335b209efd71e75e3954);
 
   pkgs = import channel-holo-host {
    overlays = [ moz-overlay ];
