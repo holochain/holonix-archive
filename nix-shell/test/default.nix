e9462f54d14530d0f49a2a1f06652bdb564d2491
Nicolas Luck <nicolas@lucksus.eu>
2019-07-06 00:45:25 +0200

    Include hc-app-spec-test-proc in hc-test

--- nix-shell/test/default.nix
+++ nix-shell/test/default.nix
@@ -4,7 +4,7 @@ let
 
   script = pkgs.writeShellScriptBin name
   ''
-  hc-rust-fmt-check && hc-qt-c-bindings-test && hc-rust-test && hc-app-spec-test
+  hc-rust-fmt-check && hc-qt-c-bindings-test && hc-rust-test && hc-app-spec-test && hc-app-spec-test-proc
   '';
 in
 script
