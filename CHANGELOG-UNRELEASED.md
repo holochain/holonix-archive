daf3f84fed9d4cd8614ea3ed291976a29ba9679f
David Meister <thedavidmeister@gmail.com>
2019-07-09 20:08:19 +1000

    fix release cutting without github release text

--- CHANGELOG-UNRELEASED.md
+++ CHANGELOG-UNRELEASED.md
@@ -14,5 +14,6 @@ This project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.htm
 
 ### Fixed
 
-### Security
+- fixed release cutting without github syncs
 
+### Security
--- release/cut/default.nix
+++ release/cut/default.nix
@@ -9,9 +9,10 @@ let
   read -r -p "Are you sure you want to cut a new release based on the current config? [y/N] " response
   case "$response" in
    [yY][eE][sS]|[yY])
-    hn-release-branch \
-    && hn-release-changelog \
-    && hn-release-push
+    hn-release-branch
+    hn-release-changelog
+    hn-release-push
+    hn-release-github
     ;;
    *)
     exit 1
