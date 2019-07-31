74a6eee5e8714e73c5730f711d13e5d413eec042
David Meister <thedavidmeister@gmail.com>
2019-07-31 20:05:08 +1000

    add git clean safety to hn-docs-github-pages

--- docs/github-pages/default.nix
+++ docs/github-pages/default.nix
@@ -5,13 +5,17 @@ let
 
   path = "docs/public";
   upstream = "origin";
-  branch = "gh-page";
+  branch = "gh-pages";
 
   # https://stackoverflow.com/questions/36782467/set-subdirectory-as-website-root-on-github-pages#36782614
   script = pkgs.writeShellScriptBin name
   ''
 set -euo pipefail
-git subtree push --prefix ${path} ${origin} ${branch}
+
+if [[ -n $(git status --porcelain) ]]
+ then echo "Repo is dirty! Commit changes before attempting to push to github pages." && exit 1
+ else git subtree push --prefix ${path} ${upstream} ${branch}
+fi
   '';
 in
 {
