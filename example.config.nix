7f6d2cc06d21cf76b04fcb0741544c2d000360f2
David Meister <thedavidmeister@gmail.com>
2019-08-05 11:52:50 +1000

    new core version

--- example.config.nix
+++ example.config.nix
@@ -68,8 +68,8 @@ echo "All finished!!!"
   # the previous version will be scanned/bumped by release scripts
   # the current version is what the release scripts bump *to*
   version = {
-   current = "0.0.22";
-   previous = "0.0.21";
+   current = "0.0.23";
+   previous = "0.0.22";
   };
 
   github = {
