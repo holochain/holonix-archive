9f02e3d7cd803a0e24abb1e5b15a7ad44ca54801
David Meister <thedavidmeister@gmail.com>
2019-08-05 11:57:03 +1000

    new core version

--- example.config.nix
+++ example.config.nix
@@ -62,14 +62,14 @@ echo "All finished!!!"
   # the commit hash that the release process should target
   # this will always be behind what ends up being deployed
   # the release process needs to add some commits for changelog etc.
-  commit = "7f6d2cc06d21cf76b04fcb0741544c2d000360f2";
+  commit = "f79e9e1369865a39c7de97e5d3255aaf1f1d2bab";
 
   # the semver for prev and current releases
   # the previous version will be scanned/bumped by release scripts
   # the current version is what the release scripts bump *to*
   version = {
-   current = "0.0.23";
-   previous = "0.0.22";
+   current = "0.0.24";
+   previous = "0.0.23";
   };
 
   github = {
