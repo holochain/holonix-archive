27654c00c976a19bf7623202014452c2c0028387
Nicolas Luck <nicolas@lucksus.eu>
2019-08-05 20:52:39 +0200

    Release 0.0.27

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,22 @@
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/).
 This project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [0.0.27] - 2019-08-05
+
+### Added
+
+### Changed
+
+- bump holochain and hc binaries to `0.0.26-alpha1`
+
+### Deprecated
+
+### Removed
+
+### Fixed
+
+### Security
+
 ## [0.0.26] - 2019-08-05
 
 ### Added
--- example.config.nix
+++ example.config.nix
@@ -62,7 +62,7 @@ echo "All finished!!!"
   # the commit hash that the release process should target
   # this will always be behind what ends up being deployed
   # the release process needs to add some commits for changelog etc.
-  commit = "4cc6f2e599df0e0ef5d15b36a8be31ba41abe4bb";
+  commit = "6793fd6d7088076948e7a0d920cfb6ad9b5f3c4e";
 
   # the semver for prev and current releases
   # the previous version will be scanned/bumped by release scripts
