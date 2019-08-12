291cd04b8e956c91f9267af61f86eb3e2cf0270a
Ashanti Mutinta <Ashantimutinta@gmail.com>
2019-08-12 13:45:17 -0400

    new core version

--- example.config.nix
+++ example.config.nix
@@ -14,7 +14,7 @@
 
    # can be any github ref
    # branch, tag, commit, etc.
-   ref = "0.0.29";
+   ref = "0.0.28";
 
    # the sha of what is downloaded from the above ref
    # note: even if you change the above ref it will not be redownloaded until
@@ -63,14 +63,14 @@ echo "All finished!!!"
   # the commit hash that the release process should target
   # this will always be behind what ends up being deployed
   # the release process needs to add some commits for changelog etc.
-  commit = "3cf2adf59afecc5d0f474365e2f7d26664ef4db9";
+  commit = "234c56becd6255e8cb5a073564f17f26e7e4b2f5";
 
   # the semver for prev and current releases
   # the previous version will be scanned/bumped by release scripts
   # the current version is what the release scripts bump *to*
   version = {
-   current = "0.0.30";
-   previous = "0.0.29";
+   current = "0.0.29";
+   previous = "0.0.28";
   };
 
   github = {
