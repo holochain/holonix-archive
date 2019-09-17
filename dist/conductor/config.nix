ca836cb0b654bd947ec3182820bc749dcb11f438
David Meister <thedavidmeister@gmail.com>
2019-09-16 21:24:34 -0400

    new core version

--- dist/conductor/config.nix
+++ dist/conductor/config.nix
@@ -2,8 +2,8 @@
  name = "conductor";
 
  sha256 = {
-  linux = "059zmqnp4vd1bv1v4k8fn2kg0z7v7ahzbmyylkjm4n9g4pw9il5s";
-  darwin = "12306846fhfdjyy5s5i6f57bbx5a7spgs40nm7r31way58zj4wkf";
+  linux = "0b6cyrhn7ns1wgr90nsf4mm2wfkxk0y68zkhv07kki19vxmpsiss";
+  darwin = "1d0yxma47agrcm937d7pzd9fm4fqjwd9kn01y7miscq0wk3yfcf7";
  };
 
  binary = "holochain";
