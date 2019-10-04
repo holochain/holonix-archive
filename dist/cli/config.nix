6bb0156b766b4ad51243565014e0199546ae6431
Willem Olding <willemolding@gmail.com>
2019-10-04 13:30:44 +1000

    fix incorrect darwin SHA

--- dist/cli/config.nix
+++ dist/cli/config.nix
@@ -3,7 +3,7 @@
 
  sha256 = {
   linux = "1cvz3n4x5r1q6syrlm6cvp9mknm2ag0jgfg0azsyhdvigq7rgwfc";
-  darwin = "196iv1yqjc0k1s2dbvfknjlw6m37gcy9swkixbyzfbk6rx0iwnfx";
+  darwin = "1q1jrqhra59y63mjil6agy1y1yqpp7jpgsxrkl878ahb291lsr7w";
  };
 
  binary = "hc";
--- dist/conductor/config.nix
+++ dist/conductor/config.nix
@@ -3,7 +3,7 @@
 
  sha256 = {
   linux = "1x1bkfyaj02zxqywyxs604b2p1zm0fbzpxr350hvlmglw0wr0qkm";
-  darwin = "0kky8npbkvrqzr8mfs25r70fdp64bb3ql875fdbvx693l04440y1";
+  darwin = "02q90qw73hl0fa1x0lj38g54b3gvp18j7pw9h5s1ss2zclprvwhv";
  };
 
  binary = "holochain";
