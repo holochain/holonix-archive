2ce8531c416cd4da464bd2aefb573d1c8cf20ce4
David Meister <thedavidmeister@gmail.com>
2019-11-06 21:37:23 +1000

    new core version

--- CHANGELOG-UNRELEASED.md
+++ CHANGELOG-UNRELEASED.md
@@ -8,6 +8,8 @@ This project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.htm
 
 ### Changed
 
+- rollback to v0.0.35-alpha7
+
 ### Deprecated
 
 ### Removed
@@ -15,4 +17,3 @@ This project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.htm
 ### Fixed
 
 ### Security
-
--- example.config.nix
+++ example.config.nix
@@ -68,8 +68,8 @@ echo "All finished!!!"
   # the previous version will be scanned/bumped by release scripts
   # the current version is what the release scripts bump *to*
   version = {
-   current = "0.0.42";
-   previous = "0.0.41";
+   current = "0.0.43";
+   previous = "0.0.42";
   };
 
   github = {
