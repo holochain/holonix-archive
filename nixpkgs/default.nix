88ba7191f59fce9b583532739e10340089fb66e2
Eric Harris-Braun <eric@harris-braun.com>
2019-11-07 14:44:42 -0500

    fixed url on github for 19.09 to point to commit not branch

--- nixpkgs/default.nix
+++ nixpkgs/default.nix
@@ -2,8 +2,8 @@ let
   # nixos channel latest 19.09
   # keep the Dockerfile in sync with this!
   nixpkgs = fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs-channels/archive/nixos-19.09.tar.gz";
-    sha256 = "15fwszhn6078sbrb8qk83g8afvh4qnmvff0qbkbvq3cm1fxni2w1";
+    url = "https://github.com/NixOS/nixpkgs-channels/tarball/821c7ed030bca86c8217e6d20df1f01c6474adf4";
+    sha256 = "0varkgzi5nbx4kb6mjmllk1a48pc5nmad6jfikj627yqrb4wcyfw";
   };
 
   # the mozilla rust overlay
