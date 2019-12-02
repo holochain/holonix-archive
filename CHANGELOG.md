1686ff4d40cac974ccc2bca78490dfa9708ed7db
David Meister <thedavidmeister@gmail.com>
2019-12-02 17:36:01 +0400

    Release v0.0.52

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,22 @@
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/).
 This project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [0.0.52] - 2019-12-02
+
+### Added
+
+### Changed
+
+### Deprecated
+
+### Removed
+
+### Fixed
+
+- fix for incorrect rust nightly version in v51
+
+### Security
+
 ## [0.0.51] - 2019-12-02
 
 ### Added
--- example.config.nix
+++ example.config.nix
@@ -62,14 +62,14 @@ echo "All finished!!!"
   # the commit hash that the release process should target
   # this will always be behind what ends up being deployed
   # the release process needs to add some commits for changelog etc.
-  commit = "21cfed80158ec7204406b19b4acf72b8fa2a6f4a";
+  commit = "eb4b1665b3b789eb67274077a42884b43dd967d1";
 
   # the semver for prev and current releases
   # the previous version will be scanned/bumped by release scripts
   # the current version is what the release scripts bump *to*
   version = {
-   current = "0.0.51";
-   previous = "0.0.50";
+   current = "0.0.52";
+   previous = "0.0.51";
   };
 
   github = {
