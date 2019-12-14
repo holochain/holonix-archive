a101fba1f127dd20314727fd7b3cf899a39e1e6d
David Meister <thedavidmeister@gmail.com>
2019-12-14 20:21:28 +0400

    holonix release

--- example.config.nix
+++ example.config.nix
@@ -62,14 +62,14 @@ echo "All finished!!!"
   # the commit hash that the release process should target
   # this will always be behind what ends up being deployed
   # the release process needs to add some commits for changelog etc.
-  commit = "b49da7974e01e5f2614f32a0142712f75e1bf186";
+  commit = "524d03c83b3766d3b47871e2bad5c98c98da2fdb";
 
   # the semver for prev and current releases
   # the previous version will be scanned/bumped by release scripts
   # the current version is what the release scripts bump *to*
   version = {
-   current = "0.0.53";
-   previous = "0.0.52";
+   current = "0.0.54";
+   previous = "0.0.53";
   };
 
   github = {
