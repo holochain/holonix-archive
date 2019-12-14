9ce1af35d0cbbd349208114dff923c93ff91ef37
David Meister <thedavidmeister@gmail.com>
2019-12-14 20:23:30 +0400

    bump example.config.nix holonix version

--- example.config.nix
+++ example.config.nix
@@ -14,14 +14,14 @@
 
    # can be any github ref
    # branch, tag, commit, etc.
-   ref = "v0.0.53";
+   ref = "v0.0.54";
 
    # the sha of what is downloaded from the above ref
    # note: even if you change the above ref it will not be redownloaded until
    #       the sha here changes (the sha is the cache key for downloads)
    # note: to get a new sha, get nix to try and download a bad sha
    #       it will complain and tell you the right sha
-   sha256 = "0ii33l1wmrcw3j8j619y8wn2wcgrl1blcb79xcifalspvjkfgm0k";
+   sha256 = "1z2pp6ba73f6n8svs0aql98d63v3prybw09rjf856xzzc34lhjga";
 
    # the github owner of the holonix repo
    owner = "holochain";
