5e5883817b277466fc92319cad79f3f58ade39f3
David Meister <thedavidmeister@gmail.com>
2020-01-17 15:37:00 +0100

    new core version

--- example.config.nix
+++ example.config.nix
@@ -62,14 +62,14 @@ echo "All finished!!!"
   # the commit hash that the release process should target
   # this will always be behind what ends up being deployed
   # the release process needs to add some commits for changelog etc.
-  commit = "d59996b48b2458d44ca08b7627867c42f671955b";
+  commit = "e6ac532efb7ac7651807c90d01eafc3ba7ef2a55";
 
   # the semver for prev and current releases
   # the previous version will be scanned/bumped by release scripts
   # the current version is what the release scripts bump *to*
   version = {
-   current = "0.0.63";
-   previous = "0.0.62";
+   current = "0.0.64";
+   previous = "0.0.63";
   };
 
   github = {
