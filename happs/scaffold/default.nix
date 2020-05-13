263614dc7cfabfdc6efcfa67cb0ab00298012d82
jetttech <lisa.jetton@holo.host>
2020-05-13 11:04:11 -0500

    updated commands and foldes to match repo updates

--- happs/scaffold/default.nix
+++ happs/scaffold/default.nix
@@ -15,28 +15,30 @@ let
     rm happ-scaffold.tar.gz
     cd ''${2:-"My-New-App"}
     sed -i "s/RAD-Tools-Phase-2/"''${2:-"My-New-App"}"/g" package.json
-    [ ! -d "./setup" ] && mkdir setup
-    curl -L -o ./setup/type-spec.json "file://$(readlink -f $(realpath --relative-to=$(pwd -P) $STARTING_DIRECTORY)/''${1})"
-    [ -f "./sample-type-spec.json" ] && rm -rf ./sample-type-spec.json
-    [ -d "./ui" ] && mv ./ui ./setup/ui-setup
-    cd ./setup/ui-setup/ui_template && npm i && cd ../../../
-    mkdir ui-src
-    mkdir keystores
-    mkdir keystores/agent1
-    cp ./.env.example ./.env
-    mv ./conductor-config.example.toml ./conductor-config.toml
-    hc keygen -n --path ./keystores/agent1/AGENT_1_PUB_KEY.keystore | sed -ne 's/^Public address:\.*//p' | xargs -I {} sh -c 'mv ./keystores/agent1/AGENT_1_PUB_KEY.keystore ./keystores/agent1/{}.keystore; sed -i "s/AGENT_1_PUB_KEY/{}/" ./conductor-config.toml'
+    [ ! -d "./src/setup" ] && mkdir ./src/setup
+    [ ! -d "./src/setup/type-specs" ] && mkdir ./src/setup/type-specs
+    curl -L -o ./src/setup/type-specs/type-spec.json "file://$(readlink -f $(realpath --relative-to=$(pwd -P) $STARTING_DIRECTORY)/''${1})"
+    [ ! -f "./src/setup/type-specs/sample-type-spec.json" ] &&  cp ./src/setup/type-specs/type-spec.json ./src/setup/type-specs/sample-type-spec.json
+    [ -d "./ui" ] && mv ./ui ./src/ui-setup
     npm i
-    npm run generate:ui
-    hc init dna-src
-    npm run hc-generate:dna
-    cd dna-src
-    hc package | sed -ne 's/^DNA hash: \.*//p' | xargs -I {} sed -i "s/DNA_HASH/{}/" ../conductor-config.toml
-    cd ../setup
-    rm -rf dna-setup
-    rm -rf ui-setup
+    npm run happ:generate ./src/setup/type-specs/type-spec.json
+    npm run hc:generate-conductor
+    rm -rf ./package.json
+    mv ./src/setup/root-package.json ./package.json
+    npm i
+    [ ! -d "./setup" ] && mkdir ./setup
+    mv ./src/setup/* ./setup
+    cp ./.env.example ./.env
+    rm -rf ./.env.example
+    rm -rf ./conductor-config.example.toml
+    rm -rf src
   '';
 in
 {
  buildInputs = [ script ];
 }
+  # mkdir keystores
+  #   mkdir keystores/agent1
+  #   hc keygen -n --path ./keystores/agent1/AGENT_1_PUB_KEY.keystore | sed -ne 's/^Public address:\.*//p' | xargs -I {} sh -c 'mv ./keystores/agent1/AGENT_1_PUB_KEY.keystore ./keystores/agent1/{}.keystore; sed -i "s/<AGENT_1_PUB_KEY>/{}/" ./conductor-config.toml'
+  #   cd dna-src
+  #   hc package | sed -ne 's/^DNA hash: \.*//p' | xargs -I {} sed -i "s/<DNA_HASH>/{}/" ../conductor-config.toml
