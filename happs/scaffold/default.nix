46c375bd158c7746efea4a0c6974feebcc50b1ec
jetttech <lisa.jetton@holo.host>
2020-05-29 02:03:31 -0500

    updated dir structural auto edits

--- happs/scaffold/default.nix
+++ happs/scaffold/default.nix
@@ -9,7 +9,7 @@ let
     ''${1?"Command Usage Error: ARG 1 - PATH TO SCHEMA REQUIRED"}
     [[ $(mimetype -b "''${1}") != "application/json" ]] && { echo "Command Usage Error: ARG 1 - JSON FILE TYPE REQUIRED"; exit 1; }
     STARTING_DIRECTORY=$(pwd -P)
-    curl -L -o happ-scaffold.tar.gz https://github.com/holochain/RAD-Tools-Phase-2/archive/merge-first-ui-w-first-dna.tar.gz
+    curl -L -o happ-scaffold.tar.gz https://github.com/holochain/RAD-Tools-Phase-2/archive/integration-testing-tryorama-fix-1.tar.gz
     mkdir "''${2:-"My-New-App"}"
     tar -zxvf happ-scaffold.tar.gz --strip-components=1 -C ./"''${2:-"My-New-App"}"
     rm happ-scaffold.tar.gz
@@ -17,18 +17,15 @@ let
     sed -i "s/RAD-Tools-Phase-2/"''${2:-"My-New-App"}"/g" package.json
     [ ! -d "./src/setup" ] && mkdir ./src/setup
     [ ! -d "./src/setup/type-specs" ] && mkdir ./src/setup/type-specs
+    [ -f "./src/setup/type-specs/type-spec.json" ] && rm -rf ./src/setup/type-specs/type-spec.json
     curl -L -o ./src/setup/type-specs/type-spec.json "file://$(readlink -f $(realpath --relative-to=$(pwd -P) $STARTING_DIRECTORY)/''${1})"
     [ ! -f "./src/setup/type-specs/sample-type-spec.json" ] &&  cp ./src/setup/type-specs/type-spec.json ./src/setup/type-specs/sample-type-spec.json
-    [ -d "./ui" ] && mv ./ui ./src/ui-setup
     npm i
     npm run happ:generate ./src/setup/type-specs/type-spec.json
-    rm -rf ./package.json
-    mv ./src/setup/root-package.json ./package.json
+    rm -rf ./src ./package.json ./config.nix ./default.nix README.md ./CHANGELOG.md ./node_modules
+    mv ./happ/* ./
+    rm -rf ./happ
     npm i
-    [ ! -d "./setup" ] && mkdir ./setup
-    mv ./src/setup/* ./setup
-    rm -rf ./conductor-config.template.toml
-    rm -rf src
   '';
 in
 {
