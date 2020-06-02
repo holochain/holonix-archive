17b8c1a498948a0c4fa86a08d724bf9f873bab37
jetttech <lisa.jetton@holo.host>
2020-06-02 15:59:13 -0500

    test against master branch

--- happs/scaffold/default.nix
+++ happs/scaffold/default.nix
@@ -3,18 +3,17 @@ let
   # ie: hc-happ-scaffold <path-to-package-file> <app-name>
   name = "hc-happ-scaffold";
 
-  # nb: currently the curling from wip branch on rad-tool-phase2. Will eventually need to repoint to master branch.
+  # nb: currently the curling from wip branch on integration-testing-tryorama-fix-1. Will eventually need to repoint to master branch, once merged.
   script = pkgs.writeShellScriptBin name
   ''
     ''${1?"Command Usage Error: ARG 1 - PATH TO SCHEMA REQUIRED"}
     [[ $(mimetype -b "''${1}") != "application/json" ]] && { echo "Command Usage Error: ARG 1 - JSON FILE TYPE REQUIRED"; exit 1; }
     STARTING_DIRECTORY=$(pwd -P)
-    curl -L -o happ-scaffold.tar.gz https://github.com/holochain/RAD-Tools-Phase-2/archive/integration-testing-tryorama-fix-1.tar.gz
+    curl -L -o happ-scaffold.tar.gz https://github.com/holochain/RAD-Tools-Phase-2/archive/master.tar.gz
     mkdir "''${2:-"My-New-App"}"
     tar -zxvf happ-scaffold.tar.gz --strip-components=1 -C ./"''${2:-"My-New-App"}"
     rm happ-scaffold.tar.gz
     cd ''${2:-"My-New-App"}
-    sed -i "s/RAD-Tools-Phase-2/"''${2:-"My-New-App"}"/g" package.json
     [ ! -d "./src/setup" ] && mkdir ./src/setup
     [ ! -d "./src/setup/type-specs" ] && mkdir ./src/setup/type-specs
     [ -f "./src/setup/type-specs/type-spec.json" ] && rm -rf ./src/setup/type-specs/type-spec.json
@@ -25,9 +24,13 @@ let
     rm -rf ./src ./package.json ./config.nix ./default.nix README.md ./CHANGELOG.md ./node_modules
     mv ./happ/* ./
     rm -rf ./happ
+
     npm i
   '';
 in
 {
  buildInputs = [ script ];
 }
+
+# sed -i "s/happ-name/"''${2:-"My-New-App"}"/g" ./package.json
+# sed -i "s/happ-name/"''${2:-"My-New-App"}"/g" ./ui-src/package.json
