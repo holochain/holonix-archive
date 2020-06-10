70c1f6dfeff6702df022f508e9a0b9464e2b130e
Joel U <joelulahanna@gmail.com>
2020-06-10 14:17:26 -0500

    only run happ on nightly (#163)
    * only run happ on nightly

--- test/default.nix
+++ test/default.nix
@@ -1,4 +1,4 @@
-{ pkgs }:
+{ pkgs, config }:
 let
  # self tests for holonix
  # mostly smoke tests on various platforms
@@ -14,7 +14,7 @@ ${if pkgs.stdenv.isLinux then "bats ./test/perf.bats" else ""}
 bats ./test/rust-manifest-list-unpinned.bats
 bats ./test/rust.bats
 bats ./test/flamegraph.bats
-bats ./test/happs.bats
+${if config.holonix.use-stable-rust == false then "bats ./test/happs.bats" else ""}
 '';
 
 in
