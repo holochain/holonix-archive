49992d2c558b2a77cca2f8808c9fa81bf3d4e3bb
zo-el <joelulahanna@gmail.com>
2020-06-10 12:47:17 -0500

    only run happ on nightly

--- test/default.nix
+++ test/default.nix
@@ -1,4 +1,4 @@
-{ pkgs }:
+{ pkgs, use-stable-rust ? false }:
 let
  # self tests for holonix
  # mostly smoke tests on various platforms
@@ -14,7 +14,7 @@ ${if pkgs.stdenv.isLinux then "bats ./test/perf.bats" else ""}
 bats ./test/rust-manifest-list-unpinned.bats
 bats ./test/rust.bats
 bats ./test/flamegraph.bats
-bats ./test/happs.bats
+${if use-stable-rust == false then "bats ./test/happs.bats" else ""}
 '';
 
 in
