34f09a4c2f467c13f8411f90fdd53f17248ad33d
Paul d'Aoust <paul.daoust@holo.host>
2020-06-25 16:11:58 -0700

    new core version

--- example.config.nix
+++ example.config.nix
@@ -68,8 +68,8 @@ echo "All finished!!!"
   # the previous version will be scanned/bumped by release scripts
   # the current version is what the release scripts bump *to*
   version = {
-   current = "0.0.77";
-   previous = "0.0.76";
+   current = "0.0.78";
+   previous = "0.0.77";
   };
 
   github = {
