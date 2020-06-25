436ec023fdec013af5245b6f44a23718e6970a71
David Meister <thedavidmeister@gmail.com>
2020-06-25 17:13:49 +0400

    new holonix blessed version

--- example.config.nix
+++ example.config.nix
@@ -68,8 +68,8 @@ echo "All finished!!!"
   # the previous version will be scanned/bumped by release scripts
   # the current version is what the release scripts bump *to*
   version = {
-   current = "0.0.75";
-   previous = "0.0.74";
+   current = "0.0.76";
+   previous = "0.0.75";
   };
 
   github = {
