ceb50c11788a7ba9667e80a66574d9a86dea9f6d
Eric Harris-Braun <eric@harris-braun.com>
2020-07-31 16:52:13 -0400

    Release v0.0.81

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,21 @@
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/).
 This project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [0.0.81] - 2020-07-31
+
+### Added
+
+### Changed
+- Updated to holochain v0.0.51-alpha1
+
+### Deprecated
+
+### Removed
+
+### Fixed
+
+### Security
+
 ## [0.0.80] - 2020-07-14
 
 ### Added
--- example.config.nix
+++ example.config.nix
@@ -62,7 +62,7 @@ echo "All finished!!!"
   # the commit hash that the release process should target
   # this will always be behind what ends up being deployed
   # the release process needs to add some commits for changelog etc.
-  commit = "2f00dfab70a1dcb1d3cb5e0247386c286b649846";
+  commit = "7ba780c927224078a6fc96fce3b2208713c821c5";
 
   # the semver for prev and current releases
   # the previous version will be scanned/bumped by release scripts
