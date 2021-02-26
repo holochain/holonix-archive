90a19d5771c069dbcc9b27938009486b54b12fb7
Stefan Junker <mail@stefanjunker.de>
2021-02-27 00:52:02 +0100

    hn-introspect: include pkg version if available

--- pkgs/holonix-introspect.nix
+++ pkgs/holonix-introspect.nix
@@ -8,7 +8,7 @@
 let
   namesVersionsString = lib.attrsets.mapAttrsToList (name: value:
     if builtins.isAttrs value && builtins.hasAttr "src" value
-    then "echo \- ${name}: " + (builtins.toString value.src.urls)
+    then "echo \- ${name}" + (if builtins.hasAttr "version" value then "-${value.version}" else "") + ": " + (builtins.toString value.src.urls)
     else ""
   ) pkgsOfInterest;
 
