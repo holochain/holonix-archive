fb15f04ea71279fadc34d81fad539ce03ba59af1
Stefan Junker <mail@stefanjunker.de>
2021-08-20 20:07:06 +0200

    manage holochain-nixpkgs revision with niv

--- config.nix
+++ config.nix
@@ -33,26 +33,6 @@ echo "<your publishing script here>"
  holochain-nixpkgs = rec {
   use-github = true;
 
-  github = rec {
-   # can be any github ref
-   # branch, tag, commit, etc.
-   ref = "26212411e9d1c9455b7a53aae12fe6ac61c681c3";
-
-   # the sha of what is downloaded from the above ref
-   # note: even if you change the above ref it will not be redownloaded until
-   #       the sha here changes (the sha is the cache key for downloads)
-   # note: to get a new sha, get nix to try and download a bad sha
-   #       it will complain and tell you the right sha
-   sha256 = "18jj5cxdn4z6qpwm03wnkpq2nzcppxz62gj6j86zyy2ydc6hidzb";
-
-   # the github owner of the holonix repo
-   owner = "holochain";
-
-   # the name of the holonix repo
-   repo = "holochain-nixpkgs";
-
-  };
-
   # configuration for when use-github = false
   local = {
    # the path to the local holonix copy
@@ -61,10 +41,7 @@ echo "<your publishing script here>"
 
   pathFn = _:
     if use-github
-    then builtins.fetchTarball (with github; {
-       url = "https://github.com/${owner}/${repo}/archive/${ref}.tar.gz";
-       inherit sha256; }
-      )
+    then (import ./nix/sources.nix).holochain-nixpkgs
     else local.path
     ;
 
--- examples/custom-holochain.nix
+++ examples/custom-holochain.nix
@@ -1,14 +1,13 @@
 # Example: Custom Holochain And Binaries
+# 
+# The following `shell.nix` file can be used in your project's root folder and activated with `nix-shell`.
+# It uses a custom revision and a custom set of binaries to be installed.
 
-The following `shell.nix` file can be used in your project's root folder and activated with `nix-shell`.
-It uses a custom revision and a custom set of binaries to be installed.
+{ 
+  holonixPath ?  builtins.fetchTarball { url = "https://github.com/holochain/holonix/archive/develop.tar.gz"; }
+}:
 
-```nix
 let
-  holonixPath = builtins.fetchTarball {
-    url = "https://github.com/holochain/holonix/archive/develop.tar.gz";
-  };
-
   holonix = import (holonixPath) {
     include = {
         # making this explicit even though it's the default
@@ -18,9 +17,9 @@ let
     holochainVersionId = "custom";
 
     holochainVersion = {
-      rev = "092df23697b7fdd53f901ec4c3a8579c280bae3f";
+      rev = "447ec6fcfc092aa6b05e39c725c155eb649fc77b";
       sha256 = "1z0y1bl1j2cfv4cgr4k7y0pxnkbiv5c0xv89y8dqnr32vli3bld7";
-      cargoSha256 = "1jiz9y1d4ybh33h1ly24s7knsqyqjagsn1gzqbj1ngl22y5v3aqh";
+      cargoSha256 = "0000000000000000000000000000000000000000000000000000";
       bins = {
         holochain = "holochain";
         hc = "hc";
@@ -29,10 +28,9 @@ let
 
       lairKeystoreHashes = {
         sha256 = "1jiz9y1d4ybh33h1ly24s7knsqyqjagsn1gzqbj1ngl22y5v3aqh";
-        cargoSha256 = "0agykcl7ysikssfwkjgb3hfw6xl0slzy38prc4rnzvagm5wd1jjv";
+        cargoSha256 = "0000000000000000000000000000000000000000000000000000";
       };
     };
   };
 
 in holonix.main
-```
