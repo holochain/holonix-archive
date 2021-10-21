d2ed5c9673f45bfd29804cc72bccb45161e9b232
Stefan Junker <mail@stefanjunker.de>
2021-10-21 21:03:24 +0200

    nix/update.sh: commit with introspect output

--- nix/update.sh
+++ nix/update.sh
@@ -1,3 +1,20 @@
 #! /usr/bin/env nix-shell 
 #! nix-shell -p niv -i bash
 niv update
+
+cat << EOF | git commit nix/sources.* -F -
+
+update holochain-nixpkgs
+
+---
+the following versions are now available via their respective holochainVersionId:
+
+$(nix-shell --pure --argstr holochainVersionId main --run 'hn-introspect hc')
+
+$(nix-shell --pure --argstr holochainVersionId develop --run 'hn-introspect hc')
+
+---
+as well as the following common commands of interest:
+
+$(nix-shell --pure --argstr holochainVersionId develop --run 'hn-introspect common')
+EOF
