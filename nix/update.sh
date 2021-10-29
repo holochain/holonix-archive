dabc01503105f39a0b65f62242af14085d78fd45
Stefan Junker <mail@stefanjunker.de>
2021-10-29 11:44:54 +0200

    move version info from git commits to VERSIONS.md

--- nix/update.sh
+++ nix/update.sh
@@ -2,19 +2,24 @@
 #! nix-shell -p niv -i bash
 niv update
 
-cat << EOF | git commit nix/sources.* -F -
+cat << EOF > VERSIONS.md
+## holochain binary versions
+the following versions are available via their respective holochainVersionId.
 
-update holochain-nixpkgs
-
----
-the following versions are now available via their respective holochainVersionId:
-
+### main
 $(nix-shell --pure --argstr holochainVersionId main --run 'hn-introspect hc')
 
+### develop
 $(nix-shell --pure --argstr holochainVersionId develop --run 'hn-introspect hc')
 
----
+## common binaries
 as well as the following common commands of interest:
 
 $(nix-shell --pure --argstr holochainVersionId develop --run 'hn-introspect common')
+EOF
+
+cat << EOF | git commit VERSIONS.md nix/sources.* -F -
+update holochain-nixpkgs
+
+see VERSIONS.md for details
 EOF
