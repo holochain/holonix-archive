66cc84592cec04de34a9846a6efe450a173042eb
Stefan Junker <mail@stefanjunker.de>
2022-02-15 16:05:35 +0100

    scaffolding: add `hn-init` wrapper script

--- scaffolding/default.nix
+++ scaffolding/default.nix
@@ -1,11 +1,17 @@
-{ sources }:
+{ sources
+, writeShellScriptBin
+}:
 
 let
   scaffolding = (import sources.scaffolding { }).holochain-create;
+  hn-init = writeShellScriptBin "hn-init" ''
+    exec ${scaffolding}/bin/holochain-create init ''${@}
+  '';
 in
 
 {
   buildInputs = [
     scaffolding
+    hn-init
   ];
 }
