32c2389b385d29cd716af199a848502072514273
Stefan Junker <mail@stefanjunker.de>
2023-01-27 15:52:46 +0100

    nix flake lock --update-input holochain

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Deprecated holochain repo (moved to https://github.com/holochain/holochain)";
 
   inputs = {
-    holochain.url = "github:holochain/holochain";
+    holochain.url = "github:holochain/holochain/pr_holonix_on_flakes";
     flake-compat = {
       url = "github:edolstra/flake-compat";
       flake = false;
